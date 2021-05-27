a0815b5717bd0389436fde5f166c9b53eaa2808d
Anastasia Maryina <nastiacool55@gmail.com>
2021-05-27 15:52:39 +0300

    changes to static

--- myapp/settings.py
+++ myapp/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'myapp', 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
