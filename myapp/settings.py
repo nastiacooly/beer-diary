2d56701265a3fe0ba79ba59574099848543b154f
Anastasia Maryina <nastiacool55@gmail.com>
2021-05-27 19:10:28 +0300

    Changes to production version for js script to work properly

--- myapp/settings.py
+++ myapp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'g*^%#kl*cv_fmd!tvrwog)py5#$twt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['beerdiary.herokuapp.com']
 
 # Application definition
 
